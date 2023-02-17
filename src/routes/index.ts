d990e48ccf75e99256b935ebd7bbc6483450268a
Tony Lopes <tonylopes@tonys-mac-mini.lan>
2023-02-17 18:10:11 -0400

    working example of get and put to sql db. Most of prisma basic setup in

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,13 @@
 import express from "express";
 var router = express.Router();
+// import { testPrisma } from "../services/db.service";
 
-router.get("/", (req: express.Request, res: express.Response) => {
+router.get("/", async (req: express.Request, res: express.Response) => {
+  // debugger;
+  // const result = await testPrisma().catch((e) => {
+  //   debugger;
+  // });
+  // debugger;
   res.send("Express + TypeScript Server");
 });
 
--- src/services/db.service.ts
+++ src/services/db.service.ts
@@ -1,6 +1,8 @@
 import { getXataClient } from "../configs/xata-custom-config";
 import { DatabaseSchema } from "../configs/xata";
-import mysql from "mysql";
+
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 const xata = getXataClient();
 
@@ -19,3 +21,31 @@ export const dbUpdate = async (
   });
   return record;
 };
+
+// const _testPrisma = async () => {
+//   // const allDataSources = await prisma["dataSource"].findMany();
+//   let allDataSources = await prisma["dataSource"].findMany();
+//   debugger;
+//   await prisma.dataSource.create({
+//     data: {
+//       name: "test",
+//       url: "test",
+//     },
+//   });
+
+//   allDataSources = await prisma["dataSource"].findMany();
+//   // debugger;
+//   // ... you will write your Prisma Client queries here
+// };
+
+// export const testPrisma = async () => {
+//   const result = await _testPrisma().catch(async (e) => {
+//     console.log("error", e);
+//     await prisma.$disconnect();
+//     process.exit(1);
+//     throw e;
+//   });
+
+//   await prisma.$disconnect();
+//   return result;
+// };
